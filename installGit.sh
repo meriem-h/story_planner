99ff62476dcfe0740b87b3f3040236cf66eddf4b
meriem-h <meriem.s.hadi@gmail.com>
2026-06-18 12:44:05 +0200

    🚀 Release 2026-06-18 12:44

--- installGit.sh
+++ installGit.sh
@@ -1,5 +1,15 @@
 source .env
 
+if [ -n "$1" ]; then
+    commit_msg="$1"
+else
+    commit_msg="🚀 Release $(date '+%Y-%m-%d %H:%M')"
+fi
+
+git add .
+git commit -m "$commit_msg"
+git push
+
 npm run make
 echo "$SUDO_PASSWORD" | sudo -S dpkg -i out/make/deb/x64/story-planner_1.0.0_amd64.deb
 echo "$SUDO_PASSWORD" | sudo -S cp .env /usr/lib/story-planner/resources/.env
